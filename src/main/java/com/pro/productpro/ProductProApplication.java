bcef684201c9484468a43b9f6ba470953e5a873e
prinaudo <prinaudo@reeturn.it>
2023-09-08 21:21:31 +0200

    ADD pom.xml

---
+++ src/main/java/com/pro/productpro/ProductProApplication.java
@@ -0,0 +1,13 @@
+package com.pro.productpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pro/productpro/ProductProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.productpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

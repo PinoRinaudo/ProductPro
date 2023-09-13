4a1bee7f6d62272e9aea7fb85f398ef4dd9fe6b5
prinaudo <prinaudo@reeturn.it>
2023-09-13 15:12:53 +0200

    FIX: pom.xml ADD:junit pom

--- src/main/java/com/pro/productpro/model/Product.java
+++ src/main/java/com/pro/productpro/model/Product.java
@@ -5,8 +5,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,45 +23,10 @@ public class Product {
 
     private double price;
 
-    // Costruttore vuoto (necessario per JPA)
-    public Product() {}
-
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
 
-    // Metodi di accesso (getter e setter)
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                '}';
-    }
 }

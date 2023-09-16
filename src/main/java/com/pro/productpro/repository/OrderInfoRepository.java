7b4a29f0933c5fc695989d359e037bf3d9715c2f
prinaudo <prinaudo@reeturn.it>
2023-09-16 17:36:44 +0200

    FIX: refactor

--- src/main/java/com/pro/productpro/repository/OrderInfoRepository.java
+++ src/main/java/com/pro/productpro/repository/OrderInfoRepository.java
@@ -3,5 +3,5 @@ package com.pro.productpro.repository;
 import com.pro.productpro.model.OrderInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<OrderInfo, Long> {
+public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
 }

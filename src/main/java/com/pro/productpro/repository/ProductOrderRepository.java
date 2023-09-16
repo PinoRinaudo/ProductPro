fcb2985580fd667e1d6e35a4e8f16924f5e7b1df
prinaudo <prinaudo@reeturn.it>
2023-09-16 17:38:57 +0200

    FIX: refactor

--- src/main/java/com/pro/productpro/repository/ProductOrderRepository.java
+++ src/main/java/com/pro/productpro/repository/ProductOrderRepository.java
@@ -1,7 +1,8 @@
 package com.pro.productpro.repository;
 
+import com.pro.productpro.compositekeys.ProductOrderId;
 import com.pro.productpro.model.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductOrderRepository extends JpaRepository<ProductOrder, Long> {
+public interface ProductOrderRepository extends JpaRepository<ProductOrder, ProductOrderId> {
 }

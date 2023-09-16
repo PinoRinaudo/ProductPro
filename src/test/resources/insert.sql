c3652e28c18da1c7126105f9022bc5f333c32da1
prinaudo <prinaudo@reeturn.it>
2023-09-16 19:03:58 +0200

    FIX .sql

--- src/test/resources/insert.sql
+++ src/test/resources/insert.sql
@@ -6,7 +6,6 @@ INSERT INTO product (price, name) VALUES (29.99, 'Prodotto B');
 INSERT INTO product (price, name) VALUES (14.99, 'Prodotto C');
 INSERT INTO product (price, name) VALUES (39.99, 'Prodotto D');
 
-
 -- Inserto into customer
 INSERT INTO customer (name) VALUES ('Customer 1');
 INSERT INTO customer (name) VALUES ('Customer 2');
@@ -16,9 +15,7 @@ INSERT INTO order_info (customer_id, date) VALUES (1, '2023-09-16 12:00:00');
 INSERT INTO order_info (customer_id, date) VALUES (1, '2023-09-17 16:00:00');
 INSERT INTO order_info (customer_id, date) VALUES (2, '2023-09-20 10:00:00');
 
-
-
 -- Inserto into order_product
 INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 1, 1);
 INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 2, 1);
-INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 2, 1);
+INSERT INTO order_product (order_id, product_id, quantity) VALUES (2, 2, 1);

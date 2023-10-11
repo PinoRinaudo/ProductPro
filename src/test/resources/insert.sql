cf3284451e61fe5300725c9e4cf6dce264b59044
Pino Rinaudo <pinorinaudo9@gmail.com>
2023-10-11 14:42:29 +0200

    FIX: .sql

--- src/test/resources/insert.sql
+++ src/test/resources/insert.sql
@@ -1,33 +1,37 @@
 USE mydatabase;
 
--- Inserto into product
-INSERT INTO product (price, name) VALUES (19.99, 'Prodotto A');
-INSERT INTO product (price, name) VALUES (29.99, 'Prodotto B');
-INSERT INTO product (price, name) VALUES (14.99, 'Prodotto C');
-INSERT INTO product (price, name) VALUES (39.99, 'Prodotto D');
+-- Insert into product
+INSERT INTO product (price, name) VALUES (29.99, 'Smartphone');
+INSERT INTO product (price, name) VALUES (49.99, 'Tablet');
+INSERT INTO product (price, name) VALUES (14.99, 'Earphones');
+INSERT INTO product (price, name) VALUES (99.99, 'Laptop');
 
--- Inserto into customer
-INSERT INTO customer (name) VALUES ('Customer 1');
-INSERT INTO customer (name) VALUES ('Customer 2');
+-- Insert into customer
+INSERT INTO customer (name) VALUES ('John Doe');
+INSERT INTO customer (name) VALUES ('Jane Smith');
+INSERT INTO customer (name) VALUES ('Bob Johnson');
 
--- Inserto into phone
-INSERT INTO phone (customer_id, number) VALUES (1, '348735492');
-INSERT INTO phone (customer_id, number) VALUES (1, '34335492');
-INSERT INTO phone (customer_id, number) VALUES (2, '348535492');
-INSERT INTO phone (customer_id, number) VALUES (2, '228735492');
+-- Insert into phone
+INSERT INTO phone (customer_id, number) VALUES (1, '5551234567');
+INSERT INTO phone (customer_id, number) VALUES (1, '5559876543');
+INSERT INTO phone (customer_id, number) VALUES (2, '5555555555');
+INSERT INTO phone (customer_id, number) VALUES (2, '5552223333');
+INSERT INTO phone (customer_id, number) VALUES (3, '5558889999');
+INSERT INTO phone (customer_id, number) VALUES (3, '5557776666');
 
--- Inserto into email
-INSERT INTO email (customer_id, email) VALUES (1, 'test1@gmail.com');
-INSERT INTO email (customer_id, email) VALUES (1, 'test2@gmail.com');
-INSERT INTO email (customer_id, email) VALUES (2, 'test3@gmail.com');
-INSERT INTO email (customer_id, email) VALUES (2, 'test4@gmail.com');
+-- Insert into email
+INSERT INTO email (customer_id, email) VALUES (1, 'john.doe@email.com');
+INSERT INTO email (customer_id, email) VALUES (1, 'johndoe@gmail.com');
+INSERT INTO email (customer_id, email) VALUES (2, 'jane.smith@email.com');
+INSERT INTO email (customer_id, email) VALUES (3, 'bob.johnson@email.com');
 
--- Inserto into order
+-- Insert into order
 INSERT INTO order_info (customer_id, date) VALUES (1, '2023-09-16 12:00:00');
 INSERT INTO order_info (customer_id, date) VALUES (1, '2023-09-17 16:00:00');
-INSERT INTO order_info (customer_id, date) VALUES (2, '2023-09-20 10:00:00');
+INSERT INTO order_info (customer_id, date) VALUES (2, '2023-09-18 10:00:00');
 
--- Inserto into order_product
-INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 1, 3);
-INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 2, 1);
+-- Insert into order_product
+INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 1, 2);
+INSERT INTO order_product (order_id, product_id, quantity) VALUES (1, 3, 1);
 INSERT INTO order_product (order_id, product_id, quantity) VALUES (2, 2, 1);
+INSERT INTO order_product (order_id, product_id, quantity) VALUES (3, 4, 1);
